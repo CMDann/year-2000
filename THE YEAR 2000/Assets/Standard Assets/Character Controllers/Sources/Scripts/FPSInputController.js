780d534e19dccfdbbb26a1c77454166e1ed53ad4
Dan(n) Blair <dann@boldapps.net>
2014-07-27 18:49:19 -0500

    Added a character, added some sick trees, added ground and a cool skybox.

--- THE YEAR 2000/Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
+++ THE YEAR 2000/Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
@@ -8,7 +8,7 @@ function Awake () {
 // Update is called once per frame
 function Update () {
 	// Get the input vector from keyboard or analog stick
-	var directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	var directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
 	
 	if (directionVector != Vector3.zero) {
 		// Get the length of the directon vector and then normalize it
